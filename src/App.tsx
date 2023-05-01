6f8fd8279ea2b5a9dd896927d1da2c2ea1c957dc
canyon <1225426443@qq.com>
2023-05-01 21:47:15 +0800

    fix:项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h3>App</h3>
+      <i className='icon-iconfont icon-zujian-wenben'></i>
+    </div>
+  )
+}
+
+export default App
